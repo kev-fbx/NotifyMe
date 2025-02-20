5078358798784d432d2ea025d03b0ccab3c5df88
kev-fbx <ktran041203@gmail.com>
2025-02-20 16:33:13 +1100

    Formatting

--- NotifyMe/static/js/script.js
+++ NotifyMe/static/js/script.js
@@ -1,19 +1,16 @@
-function processDB() {
-    fetch('/process_DB', {
+function transferData() {
+  fetch('/transfer', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+          'Content-Type': 'application/json'
       },
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.status === 'success') {
-        alert('Data processed successfully');
-      } else {
-        alert('Data processing failed');
-      }
-    })
-    .catch(error => {
+  })
+  .then(response => response.json())
+  .then(data => {
+      alert(data.message);
+  })
+  .catch(error => {
       console.error('Error:', error);
+      alert("An error occurred during transfer.");
   });
 }
